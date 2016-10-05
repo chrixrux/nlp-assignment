873e8ae267b78d5b1b4e65b915b5a8f96249fd25
Christian Widmer <christian.widmer@tum.de>
2016-10-05 12:18:47 +0800

    Add Stemming

---
+++ src/NormalizationResult.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class NormalizationResult {
+	List<String> tokenList;
+	Map<String, Integer> tokenCountMap;
+	int uniqueTokensCount;
+	
+	NormalizationResult(List<String> tokenList, Map<String, Integer> tokenCountMap) {
+		this.tokenList = tokenList;
+		this.tokenCountMap = tokenCountMap;
+		this.uniqueTokensCount = tokenCountMap.size();	
+	}
+	
+	/**
+	 * This will print a table of the n most frequent words
+	 * @param n number of tokens to print
+	 */
+	public void printTopNTokens(int n) {
+		Map<String, Integer>  sortedTokenCountMap = Utils.sortMapByValueDescending(tokenCountMap);
+		System.out.println("Top " + n + " Words after stop words were removed and before the stemming");
+		int counter = 0;
+		for (Entry<String, Integer> entry : sortedTokenCountMap.entrySet()) {
+		    String key = entry.getKey();
+		    Integer value = entry.getValue();
+		    System.out.printf("Position: %-3s Token: %-10s Count: %-3s%n",
+		    		(counter + 1), key, value);
+		    counter++;
+		    if (counter >= n) {
+		    	break;
+		    }
+		}
+	}
+}
---
+++ src/Utils.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class contains various utils functions
+ * 
+ * @author christian
+ *
+ */
+public class Utils {
+	
+	//This code is taken from: http://stackoverflow.com/questions/109383/sort-a-mapkey-value-by-values-java
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortMapByValueDescending(Map<K, V> map) {
+		List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+			public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+
+		Map<K, V> result = new LinkedHashMap<K, V>();
+		for (Map.Entry<K, V> entry : list) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+		return result;
+	}
+
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortMapByValueAscending(Map<K, V> map) {
+		List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+			public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+				return (o1.getValue()).compareTo(o2.getValue());
+			}
+		});
+
+		Map<K, V> result = new LinkedHashMap<K, V>();
+		for (Map.Entry<K, V> entry : list) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+		return result;
+	}
+	
+	public static <E> List<E> getNRandomListElements(List<E> list, int numberOfElements) {
+		if (list.size() < numberOfElements) {
+			numberOfElements = list.size();
+		}
+		List<E> resultList = new ArrayList<>(numberOfElements);
+		for (int i = 0; i < numberOfElements; i++) {
+			int index = (int) (Math.random()*list.size());
+			E selectedElement = list.remove(index);
+			resultList.add(selectedElement);
+		}
+		return resultList;
+	}
+}
